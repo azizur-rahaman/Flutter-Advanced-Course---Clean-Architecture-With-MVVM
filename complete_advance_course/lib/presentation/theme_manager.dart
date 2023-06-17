6ab3b8eb0d13812a9a3039832eb9836e3ce88ea1
Azizur Rahaman <frazizur.rahaman@gmail.com>
2023-06-17 20:30:15 +0600

    working on edit profile

--- complete_advance_course/lib/presentation/theme_manager.dart
+++ complete_advance_course/lib/presentation/theme_manager.dart
@@ -1,28 +1,80 @@
 import 'package:complete_advance_course/presentation/color_manager.dart';
+import 'package:complete_advance_course/presentation/font_manager.dart';
+import 'package:complete_advance_course/presentation/styles_manager.dart';
 import 'package:complete_advance_course/presentation/value_manager.dart';
 import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-    // main colors of the app
-    primaryColor: ColorManager.primary,
-    primaryColorLight: ColorManager.primary.withOpacity(0.7),
-    primaryColorDark: ColorManager.darkPrimary,
-    disabledColor: ColorManager.grey1, // this will be use for siabled button
+      // main colors of the app
+      primaryColor: ColorManager.primary,
+      primaryColorLight: ColorManager.primary.withOpacity(0.7),
+      primaryColorDark: ColorManager.darkPrimary,
+      disabledColor: ColorManager.grey1, // this will be use for siabled button
 
-    // card view theme
-    cardTheme: CardTheme(
-      color: ColorManager.white,
-      shadowColor: ColorManager.grey,
-      elevation: AppSize.s4,
-    )
+      //ripple color
+      splashColor: ColorManager.primary.withOpacity(0.7),
 
-    // App bar theme
+      // card view theme
+      cardTheme: CardTheme(
+        color: ColorManager.white,
+        shadowColor: ColorManager.grey,
+        elevation: AppSize.s4,
+      ),
 
-    // Button theme
+      // App bar theme
+      appBarTheme: AppBarTheme(
+          centerTitle: true,
+          color: ColorManager.grey,
+          elevation: AppSize.s4,
+          shadowColor: ColorManager.primary.withOpacity(0.7),
+          titleTextStyle: getRegularStyle(
+              color: ColorManager.white,
+              fontSize: FontSize.s16)), // Button theme
+      buttonTheme: ButtonThemeData(
+        shape: const StadiumBorder(),
+        disabledColor: ColorManager.grey1,
+        buttonColor: ColorManager.primary,
+        splashColor: ColorManager.primary.withOpacity(0.7),
+      ),
 
-    // Text theme
-
-    // input description theme (text form field)
-  );
+      // elevated button theme
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              textStyle: getRegularStyle(color: ColorManager.white),
+              backgroundColor: ColorManager.primary,
+              shape: RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.circular(AppSize.s12)))), // Text theme
+      textTheme: TextTheme(
+        displayLarge: getSemiBoldStyle(
+            color: ColorManager.darkGrey, fontSize: FontSize.s16),
+        titleMedium: getMediumStyle(
+            color: ColorManager.lightGrey, fontSize: FontSize.s14),
+        bodySmall: getRegularStyle(color: ColorManager.grey1),
+        bodyLarge: getRegularStyle(color: ColorManager.grey),
+      ),
+      // input description theme (text form field)
+      inputDecorationTheme: InputDecorationTheme(
+        contentPadding: EdgeInsets.all(AppPadding.p8),
+        hintStyle: getRegularStyle(color: ColorManager.grey1),
+        labelStyle: getRegularStyle(color: ColorManager.darkGrey),
+        errorStyle: getRegularStyle(color: ColorManager.error),
+        enabledBorder: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: ColorManager.grey, width: AppSize.s1_5),
+            borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8))),
+        focusedBorder: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: ColorManager.primary, width: AppSize.s1_5),
+            borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8))),
+        errorBorder: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: ColorManager.error, width: AppSize.s1_5),
+            borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8))),
+        focusedErrorBorder: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: ColorManager.primary, width: AppSize.s1_5),
+            borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8))),
+      ));
 }
